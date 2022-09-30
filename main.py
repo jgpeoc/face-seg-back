411d7e90949ee3cdb4c0b6edc1fa56ab8596ef15
jgpeoc <jgpeoc@gmail.com>
2022-09-29 18:07:06 -0700

    health endpoint done

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello AWS World!")
+from email.base64mime import header_length
+#main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")
+async def health():
+    return {"message": "Service is online"}
